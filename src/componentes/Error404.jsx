7398fd6dc114965598236cf74062ecb9b5029ae0
soymaku <g7anelli@gmail.com>
2023-01-23 22:37:32 -0300

    modificado visuales del navbar

--- src/componentes/Error404.jsx
+++ src/componentes/Error404.jsx
@@ -8,7 +8,6 @@ const Error404 = () => {
         <div className="col text-center">
           <h1>Error 404</h1>
           <h3>La página que estas buscando no existe. <Link to={"/"}>Volver a la pagina principal</Link></h3>
-
         </div>
       </div>
     </div>
--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -5,7 +5,7 @@ import CartWidget from './CartWidget';
 const Navbar = () => {
   return(
     <div className="container">
-      <div className='row'>
+      <div className='row text-center d-flex justify-content-center'>
         <div className="col-md-6">
           <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
@@ -13,7 +13,7 @@ const Navbar = () => {
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
-              <div className="collapse navbar-collapse" id="navbarNav">
+              <div className="collapse navbar-collapse text-start" id="navbarNav">
                 <ul className="navbar-nav">
                   <li className="nav-item">
                     <NavLink className="nav-link" aria-current="page" to={"/category/cuadros"}>Cuadros</NavLink>
@@ -28,12 +28,12 @@ const Navbar = () => {
                     <NavLink className="nav-link" to={"/category/stems"}>Stems</NavLink>
                   </li>
                 </ul>
+                <div className=' d-flex justify-content-center align-items-center'>
+                  <CartWidget />
+                </div>
               </div>
             </div>
-          </nav>
-        </div>
-        <div className='col-md-6 d-flex justify-content-end align-items-center'>
-          <CartWidget />
+          </nav>  
         </div>
       </div>
     </div>
